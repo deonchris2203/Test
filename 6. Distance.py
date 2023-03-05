fa60f916069f7c6db2fa35f85ba128732a32e0ed
Deon Fernandes <69762081+deonchris2203@users.noreply.github.com>
2023-03-05 21:38:50 +0530

    Add files via upload

---
+++ 6. Distance.py
@@ -0,0 +1,13 @@
+#Program to convert Kilometers to miles and vice versa
+print("1.Convert Kilometers to Miles\n" , "2.Convert Miles to Kilometers\n")
+a=int(input("Choose either 1 or 2: "))
+if(a==1):
+    z=int(input("Enter distance in Kilometers: "))
+    km=z*0.621371
+    print("THe distance in miles is: ", km )
+elif(a==2):
+    y=int(input("Enter distance in Miles: "))
+    mil=y/0.62137119
+    print("The distance in Kilometers is: ", mil)
+else:
+    print("That is not a option! Please choose again")
